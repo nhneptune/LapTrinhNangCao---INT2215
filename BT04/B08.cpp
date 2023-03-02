7169a9e12d6f5f910b710df66fd8eff78eb7c7cb
nhneptune <124415548+nhneptune@users.noreply.github.com>
2023-03-02 18:21:23 +0700

    bai tap tuan 5

---
+++ BT04/B08.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int nam[n], nu[n];
+    for (int i = 0; i < n; i++){
+        cin >> nam[i];
+        cin >> nu[i];
+    }
+
+    
+    return 0;
+}
+
+    
